f8b6637d645c30c7d49827d143fdcfd46930d42a
Akib Ali <akib7599@gmail.com>
2022-08-17 13:47:39 +0530

    commit 17 -aug-1:47

---
+++ db.js
@@ -0,0 +1,26 @@
+[
+    {
+        "Id":1,
+      "category": "4star",
+      "image_url": "",
+      "type_of_room": "6",
+      "bed_type": "double",
+          "no_of_persons":"6",
+          "capacity":"6",
+          "cost":"60000",
+          "booked":"false"
+    },
+
+    {
+        "Id":2,
+      "category": "3star",
+      "image_url": "",
+      "type_of_room": "4",
+      "bed_type": "double",
+          "no_of_persons":"4",
+          "capacity":"6",
+          "cost":"60000",
+          "booked":"false"
+    },
+
+  ]
